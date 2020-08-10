74f6bbe7b3c41abafc97fa22443ed4a1ab371301
Staik <oStaiko45@gmail.com>
2020-08-10 03:27:33 -0500

    Changed project name, Updated Metronome script
    Project name changes to NullSpaceDive
    
    Metronome script optimized and cleaned up according to project goals. Added new handlers to allow the bpm and beat counts to be updates during runtime, through the editor or external scripts.

--- Assets/Scripts/Metronome.cs
+++ Assets/Scripts/Metronome.cs
@@ -1,14 +1,14 @@
 ﻿using NaughtyAttributes;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Metronome : MonoBehaviour
 {
     public static Metronome instance;
 
-    public BeatMode mode = BeatMode.Four;
-    public OffBeatMode offMode = OffBeatMode.Doubles;
+    [MinValue(1)]
+    public int beats = 4;
+    [OnValueChanged("ApplyOffBeats")] [MinValue(0),MaxValue(4)]
+    public int offBeats = 1;
 
 
     [OnValueChanged("ApplyBPM")] [MinValue(30),MaxValue(240)] [SerializeField]
@@ -17,12 +17,16 @@ public class Metronome : MonoBehaviour
     private float timeBetweenBeats = default; // (s/beat)
 
 
-    public delegate void OnBeat();
-    public static event OnBeat onBeat;
-    public delegate void OffBeat();
-    public static event OffBeat offBeat;
-    public delegate void OnBeat1();
-    public static event OnBeat1 onBeat1;
+    public delegate void onBeat();
+    public static event onBeat OnBeat;
+    public delegate void offBeat();
+    public static event offBeat OffBeat;
+    public delegate void newBar();
+    public static event newBar NewBar;
+    public delegate void onBeatLast();
+    public static event onBeatLast OnBeatLast;
+    public delegate void offBeatLast();
+    public static event offBeatLast OffBeatLast;
 
 
     public bool active;
@@ -40,13 +44,16 @@ public class Metronome : MonoBehaviour
     public Transform beatGraphic = default;
 
 
-
     private float lastBeat = 0; // (s)
     private float pauseOffset = 1; // (s)
 
     void Awake()
     {
+        active = false;
         ApplyBPM();
+        previousOffBeats = offBeats;
+        previousTimeBetweenBeats = timeBetweenBeats;
+        active = true;
     }
 
     void Start()
@@ -59,211 +66,48 @@ public class Metronome : MonoBehaviour
 
     void OnEnable()
     {
-        InvokeRepeating("Beat", pauseOffset, timeBetweenBeats / ((int)offMode+1));
+        InvokeRepeating("Beat", pauseOffset, timeBetweenBeats / (offBeats + 1));
         if (showTiming) Debug.Log("Enabling Metronome\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
     }
 
     void OnDisable()
     {
-        pauseOffset = (timeBetweenBeats/((int)offMode+1)) - (Time.time-lastBeat);
+        pauseOffset = (timeBetweenBeats/(offBeats+1)) - (Time.time-lastBeat);
         CancelInvoke();
         if (showTiming) Debug.Log("Disabling Metronome\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
     }
 
     protected int lastMainIndex = int.MaxValue-1;
-    protected int lastSubIndex = int.MaxValue-1;
+    protected int lastOffIndex = int.MaxValue-1;
 
-    protected void Beat ()
+    protected void Beat () // Sequences beats in the propper order and triggers their events. Invoked by OnEnable()
     {
         lastBeat = Time.time;
 
-        if (++lastSubIndex > (int)offMode)
+        if (showTiming) Debug.Log(lastMainIndex + "-" + lastOffIndex + ":\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
+        if (visualizeBeat && beatGraphic) if (beatGraphic.localScale == Vector3.one) beatGraphic.localScale *= 2; else beatGraphic.localScale = Vector3.one; // Displays the beat on-screen directly
+
+        if (++lastOffIndex > offBeats) // Next Main Beat
         {
-            lastSubIndex = 0;
-            if (++lastMainIndex > (int)mode)
-                lastMainIndex = 0;
-        }
-
-        TriggerBeat((BeatType)((lastMainIndex * 5) + lastSubIndex));
-    }
-
-    void TriggerBeat (BeatType bt)
-    {
-        if (showTiming) Debug.LogFormat(bt + "\n" + lastMainIndex + "-" + lastSubIndex + ":\t" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-        if (visualizeBeat && beatGraphic) if (beatGraphic.localScale == Vector3.one) beatGraphic.localScale *= 2; else beatGraphic.localScale = Vector3.one;
-
-
-        if (bt >= BeatType._______)
-        {
-            Debug.LogWarning("Generic BeatTypes not supported!");
-            return;
-        }
-    }
-
-    
-
-    /*protected void Beat()
-    {
-        lastBeat = Time.time;
-
-        lastSubIndex++;
-        if (lastSubIndex > (int)offMode) // Main Beats
-        {
-            lastSubIndex = 0;
-            lastMainIndex++;
-
-            onBeat?.Invoke();
-
-            if (lastMainIndex > (int)mode) // New Bar
+            lastOffIndex = 0;
+            if (++lastMainIndex > beats) // Next Bar
             {
                 lastMainIndex = 0;
-
-                onBeat1?.Invoke();
-                Debug.Log("Beat1!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
+                NewBar?.Invoke();
             }
-            else
-            {
-                switch (lastMainIndex)
-                {
-                    case 1:
-                        //onBeat2?.Invoke();
-                        Debug.Log("Beat2!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 2:
-                        //onBeat3?.Invoke();
-                        Debug.Log("Beat3!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 3:
-                        //onBeat4?.Invoke();
-                        Debug.Log("Beat4!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 4:
-                        //onBeat5?.Invoke();
-                        Debug.Log("Beat5!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 5:
-                        //onBeat6?.Invoke();
-                        Debug.Log("Beat6!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 6:
-                        //onBeat7?.Invoke();
-                        Debug.Log("Beat7!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                    case 7:
-                        //onBeat8?.Invoke();
-                        Debug.Log("Beat8!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                        break;
-                }
-            }
+            OnBeat?.Invoke();
+            if (lastMainIndex == beats)
+                OnBeatLast?.Invoke();
         }
-        else // Sub Beats
+        else // Next Off Beat
         {
-            offBeat?.Invoke();
-
-            //TODO Invert nested switch statements for blanket lettering event calls
-
-            switch (lastMainIndex)
-            {
-                case 0:
-                    switch (lastSubIndex)
-                    {
-                        case 1:
-                            //offBeat1?.Invoke();
-                            Debug.Log("OffBeat1!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 2:
-                            //offBeat1b?.Invoke();
-                            Debug.Log("OffBeat1b!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 3:
-                            //offBeat1c?.Invoke();
-                            Debug.Log("OffBeat1c!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 4:
-                            //offBeat1d?.Invoke();
-                            Debug.Log("OffBeat1d!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        default:
-                            Debug.LogWarning("Overload not supported!" + lastSubIndex);
-                            break;
-                    }
-                    break;
-                case 1:
-                    switch (lastSubIndex)
-                    {
-                        case 1:
-                            //offBeat2?.Invoke();
-                            Debug.Log("OffBeat2!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 2:
-                            //offBeat2b?.Invoke();
-                            Debug.Log("OffBeat2b!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 3:
-                            //offBeat2c?.Invoke();
-                            Debug.Log("OffBeat2c!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 4:
-                            //offBeat2d?.Invoke();
-                            Debug.Log("OffBeat2d!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                    }
-                    break;
-                case 2:
-                    switch (lastSubIndex)
-                    {
-                        case 1:
-                            //offBeat3?.Invoke();
-                            Debug.Log("OffBeat3!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 2:
-                            //offBeat3b?.Invoke();
-                            Debug.Log("OffBeat3b!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 3:
-                            //offBeat3c?.Invoke();
-                            Debug.Log("OffBeat3c!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 4:
-                            //offBeat3d?.Invoke();
-                            Debug.Log("OffBeat3d!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                    }
-                    break;
-                case 3:
-                    switch (lastSubIndex)
-                    {
-                        case 1:
-                            //offBeat4?.Invoke();
-                            Debug.Log("OffBeat4!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 2:
-                            //offBeat4b?.Invoke();
-                            Debug.Log("OffBeat4b!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 3:
-                            //offBeat4c?.Invoke();
-                            Debug.Log("OffBeat4c!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                        case 4:
-                            //offBeat4d?.Invoke();
-                            Debug.Log("OffBeat4d!\n" + System.DateTime.Now.Second + "." + System.DateTime.Now.Millisecond);
-                            break;
-                    }
-                    break;
-                default:
-                    Debug.LogWarning("Overload not supported!" + lastMainIndex);
-                    break;
-            }
+            OffBeat?.Invoke();
+            if (lastOffIndex == offBeats/* && lastMainIndex == beats*/)
+                OffBeatLast?.Invoke();
         }
-    }*/
+    }
 
-
-
-
-        
-
-    public bool SetBPM(float newBPM)
+    public bool SetBPM(float newBPM) // Public method for other classes to change the obpm
     {
         if (newBPM < 30 || newBPM > 240)
             return false;
@@ -273,52 +117,68 @@ public class Metronome : MonoBehaviour
         return true;
     }
 
-    protected void ApplyBPM()
+    protected float previousTimeBetweenBeats = 0;
+    protected void ApplyBPM() // NaughtyAttributes handler for changing beats during runtime through the Unity Editor
     {
+        bpm = Mathf.Clamp(bpm, 30, 240);
         timeBetweenBeats = (60 / bpm);
+
+        if (UnityEditor.EditorApplication.isPlaying && active)
+        {
+            CancelInvoke();
+            float offset = (previousTimeBetweenBeats / (offBeats + 1)) - (Time.time - lastBeat);
+            InvokeRepeating("Beat", offset, timeBetweenBeats / (offBeats + 1));
+        }
+        previousTimeBetweenBeats = timeBetweenBeats;
     }
 
-    public bool SetBeatMode (BeatMode bm)
+    public bool SetBeats (int n) // Public method for other classes to change the beat count
     {
-        if (bm == mode)
+        if (n < 1)
             return false;
 
-        mode = bm;
+        beats = n;
         return true;
     }
 
-    public bool SetOffBeatMode(OffBeatMode obm)
+
+    public bool SetOffBeats(int n) // Public method for other classes to change the offbeat count
     {
-        if (obm == offMode)
+        if (n < 0 || n > 4)
             return false;
 
-        offMode = obm;
+        offBeats = n;
+        ApplyOffBeats();
         return true;
+    }
+
+    protected int previousOffBeats = 0;
+    public void ApplyOffBeats() // NaughtyAttributes handler for changing offbeats during runtime through the Unity Editor
+    {
+        if (!UnityEditor.EditorApplication.isPlaying)
+            return;
+
+        offBeats = Mathf.Clamp(offBeats, 0, 4);
+
+        CancelInvoke();
+        float offset = (timeBetweenBeats / (previousOffBeats + 1)) - (Time.time - lastBeat);
+        InvokeRepeating("Beat", offset, timeBetweenBeats / (offBeats+1));
+        previousOffBeats = offBeats;
     }
 }
 
-public enum BeatMode
-{
-    One = 0,
-    Two = 1,
-    Three = 2,
-    Four = 3,
-    Five = 4,
-    Six = 5,
-    Seven = 6,
-    Eight = 7
-}
-
-public enum OffBeatMode
-{
-    Singles = 0,
-    Doubles = 1,
-    Triplets = 2,
-    Quadruplets = 3,
-    Quintuplets = 4
-}
 
 public enum BeatType
+{
+    OnBeat = 1,
+    OffBeat = 2,
+    NewBar = 3,
+    OnBeatLast = 4,
+    OffBeatLast = 5,
+}
+
+
+/*public enum BeatType
 {
     OnBeat1 = 0,
     OffBeat1 = 1,
@@ -369,57 +229,4 @@ public enum BeatType
     OffBeatb = 46,
     OffBeatc = 47,
     OffBeatd = 48,
-}
-
-
-/*public enum BeatType
-{
-    OnBeat = 0,
-    OnBeat1 = 1,
-    OnBeat2 = 2,
-    OnBeat3 = 3,
-    OnBeat4 = 4,
-    OnBeat5 = 5,
-    OnBeat6 = 6,
-    OnBeat7= 7,
-    OnBeat8 = 8,
-    OnBeatLast = 9,
-    OffBeat = 10,
-    OffBeat1 = 11,
-    OffBeat1b = 12,
-    OffBeat1c = 13,
-    OffBeat1d = 14,
-    OffBeat2 = 15,
-    OffBeat2b = 16,
-    OffBeat2c = 17,
-    OffBeat2d = 18,
-    OffBeat3 = 19,
-    OffBeat3b = 20,
-    OffBeat3c = 21,
-    OffBeat3d = 22,
-    OffBeat4 = 23,
-    OffBeat4b = 24,
-    OffBeat4c = 25,
-    OffBeat4d = 26,
-    OffBeat5 = 27,
-    OffBeat5b = 28,
-    OffBeat5c = 29,
-    OffBeat5d = 30,
-    OffBeat6 = 31,
-    OffBeat6b = 32,
-    OffBeat6c = 33,
-    OffBeat6d = 34,
-    OffBeat7 = 35,
-    OffBeat7b = 36,
-    OffBeat7c = 37,
-    OffBeat7d = 38,
-    OffBeat8 = 39,
-    OffBeat84b = 40,
-    OffBeat8c = 41,
-    OffBeat8d = 42,
-    OffBeata = 43,
-    OffBeatb = 44,
-    OffBeatc = 45,
-    OffBeatd = 46,
-    OffBeatLast = 47
 }*/
