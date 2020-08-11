431d470266079c6fc0660ca611d71c319da3e480
MJakeShapiro <49694168+MJakeShapiro@users.noreply.github.com>
2020-08-11 14:01:43 -0500

    Set up RandomDoorAssignment
    Known Bugs:
    --a door is not assigned a nextDoor.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Door : MonoBehaviour
+{
+    public bool visited;
+    public Door nextDoor;
+
+    private void OnEnable()
+    {
+        MapGenerator.Instance.allDoors.Add(this);
+    }
+
+}
